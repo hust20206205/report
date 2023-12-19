d50ea6d2bb97210cca58010c69315164c2d34562
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-12-19 21:48:24 +0700

    clm

--- baocao/tools/zViewGit.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-
-pyautogui.hotkey('ctrl', 'k')
-pyautogui.hotkey('ctrl', 'w')
-pyautogui.hotkey('ctrl', 'shift', 'g')
-pyautogui.hotkey('ctrl', '9')
-pyautogui.hotkey('ctrl', 'j')
